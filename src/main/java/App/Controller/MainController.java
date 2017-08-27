61109afd06092a017e1307abdc5a6083bc073336
Username <mail@mail.com>
2017-08-27 14:54:42 +0200

    POST method

--- src/main/java/App/Controller/MainController.java
+++ src/main/java/App/Controller/MainController.java
@@ -2,6 +2,7 @@ package App.Controller;
 
 import App.Model.WarehouseStock;
 import App.Model.WarehouseStockRepo;
+import App.Service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,9 @@ public class MainController {
     @Autowired
     WarehouseStockRepo warehouseStockRepo;
 
+    @Autowired
+    ProductService productService;
+
 
     @RequestMapping(value = "/stock", method = RequestMethod.GET)
     public Iterable<WarehouseStock> getStock() {
@@ -23,6 +27,12 @@ public class MainController {
     @RequestMapping(value = "/stock/{ID}", method = RequestMethod.GET)
     public WarehouseStock getStockByID(@PathVariable(value = "ID") Integer productID) {
         return warehouseStockRepo.findByProductid(productID);
+    }
+
+    @RequestMapping(value = "/stock/new", method = RequestMethod.POST)
+    public void createNewProduct(@RequestParam String productName, @RequestParam String packagingType,
+                                 @RequestParam String size, @RequestParam Integer quantity) {
+        productService.createNewProduct(productName, packagingType, size, quantity);
     }
 
 }
